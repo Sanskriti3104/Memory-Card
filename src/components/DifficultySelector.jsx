3d9f406f7c837a8206e14a4c4a144e09b3d0bf1e
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-05-27 02:32:48 +0530

    Add classname for each difficulty options

--- src/components/DifficultySelector.jsx
+++ src/components/DifficultySelector.jsx
@@ -2,19 +2,27 @@ function DifficultySelector({ difficulty, setDifficulty }) {
   return (
     <div className="difficulty-selector">
       <button
-        className="difficulty-option"
+        className={`difficulty-option ${
+          difficulty === "easy" ? "active" : ""
+        }`}
         onClick={() => setDifficulty("easy")}
       >
         Easy
       </button>
+
       <button
-        className="difficulty-option"
+        className={`difficulty-option ${
+          difficulty === "medium" ? "active" : ""
+        }`}
         onClick={() => setDifficulty("medium")}
       >
         Medium
       </button>
+
       <button
-        className="difficulty-option"
+        className={`difficulty-option ${
+          difficulty === "hard" ? "active" : ""
+        }`}
         onClick={() => setDifficulty("hard")}
       >
         Hard
